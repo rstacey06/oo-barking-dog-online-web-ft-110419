a6b8093da19398e2a01242ad43ec5c08b8cdad12
Burbage Stacey <rstacey06@gmail.com>
2019-11-11 15:29:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,8 @@
 class Dog 
-  def name= (dog_name)
-    @this_dog_name = dog_name 
-  end
   def dog_name 
     @this_dog_name
+  def name= (dog_name)
+    @this_dog_name = dog_name 
   end
   def bark
     puts "woof!"
